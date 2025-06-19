82dbaf989f8ee3a79f1e3cfc64c65f15c6b24573
Kaoru Matsunaga <m.kaoru0830@gmail.com>
2025-06-19 22:36:17 +0900

    class

---
+++ pokemon.py
@@ -0,0 +1,17 @@
+class LiverpoolTeamMenber:
+    
+    def __init__(self):
+        self.name = "遠藤航"
+        self.Pow = 60
+        self.Tec = 60
+        self.Pas = 70
+        self.Phy = 80
+        self.Sho = 70
+        self.Dfc = 80
+
+
+wataru = LiverpoolTeamMenber()
+name = wataru.name
+Pow = wataru.Pow
+
+print(name,Pow)
